47856738cd33b0626c3e7c8d534f2adb728928cd
Palak Sirari <palaksirari321s@gmail.com>
2026-09-07 09:36:28 +0530

    backbutton added

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-const BASE_URL = 'https://momento-p7t1.onrender.com';
 
+const BASE_URL = 'https://momento-p7t1.onrender.com/api';
 
 export const clientServer = axios.create({
     baseURL : BASE_URL,
