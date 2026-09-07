5605b64cef82f1ddc134f38400c3f08795cbc055
Palak Sirari <palaksirari321s@gmail.com>
2026-09-07 10:05:27 +0530

    backend deployed

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 
-const BASE_URL = 'https://momento-p7t1.onrender.com';
-
+// const BASE_URL = 'https://momento-p7t1.onrender.com'
+const BASE_URL = 'http://localhost:8000';
 export const clientServer = axios.create({
     baseURL : BASE_URL,
     withCredentials : true
